60bdaed2f3a68818c8acedbfff35f25bcfdc5941
Lydia-09 <pasky0901@gmail.com>
2021-11-17 21:43:52 +0800

    feat: add bootstrap & modify main.hbs and index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
 const Record = require('./models/record')
+const Category = require('./models/category')
 
 require('./config/mongoose')
 const app = express()
@@ -13,6 +14,7 @@ app.set('views', './views')
 app.get('/', (req, res) => {
   Record.find()
     .lean()
+    .populate('categoryId')
     .then(records => res.render('index', { records }))
     .catch(error => console.error(error))
 })
