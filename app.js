c37ffd3c4f247ca867bbb1e4c85143838c7e0818
Lydia-09 <pasky0901@gmail.com>
2021-11-18 17:10:32 +0800

    feat: add method-override and update function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const routes = require('./routes')
 const app = express()
@@ -8,11 +9,12 @@ const app = express()
 require('./config/mongoose')
 const port = 3000
 
-app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs', helpers: require('./config/helpers') }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use(routes)
 
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+const record = require('../../models/record')
 
 // 新增支出
 router.get('/new', async (req, res) => {
-   const category = await Category.find().lean()
+  const category = await Category.find().lean()
   return res.render('new', { category })
 })
 
@@ -22,12 +23,27 @@ router.post('/', async (req, res) => {
 })
 
 // Read - 瀏覽特定支出
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const id = req.params.id
-  return Record.findById(id)
+  await Record.findById(id)
     .lean()
     .then(record => res.render('detail', { record }))
     .catch((error) => console.log(error))
+})
+
+// Update - 修改特定支出
+router.get('/:id/edit', async (req, res) => {
+    const id = req.params.id  
+    const record = await Record.findById(id).lean()
+    const categoryData = await Category.find().select('name').lean() 
+    return res.render('edit', { record,  categoryData})
+})
+
+// router.post('/:id/edit', (req, res) => {
+router.put('/:id', (req, res) => {  
+    const id = req.params.id
+    Record.findByIdAndUpdate(id, { $set: req.body })
+    .then(()=> res.redirect('/'))
 })
 
 module.exports = router
