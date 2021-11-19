b467f33fa7b60a4a50dbf790c18c028b513927c2
Lydia-09 <pasky0901@gmail.com>
2021-11-19 09:52:13 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,10 +4,25 @@ const Record = require('../../models/record')
 const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
+  // 取得排序選項
+  const sortValueString = req.query.sort
+  // 排序選項對應查詢
+  const sortOption = {
+    amount: {amount: 'desc'},
+    lessAmount: {amount: 'asc'},
+    newDate: {date: 'desc'},
+    oldDate: {date: 'asc'},
+    name: {name: 'asc'},
+    category: {categoryId: 'asc'}
+  }
+  // 提供 boolean 資訊給 handlebars helper, 設定 sort 選項
+  const sort = sortValueString ? { [sortValueString]: true } : { 'id': true }
+
   Record.find()
     .lean()
     .populate('categoryId')
-    .then(records => res.render('index', { records }))
+    .sort(sortOption[sortValueString])
+    .then(records => res.render('index', { records, sort }))
     .catch(error => console.error(error))
 })
 
